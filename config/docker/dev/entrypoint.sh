b729d1deb76c3aa8685b79024afc7f320456fcfe
Nick Gronow <nick@wov.io>
2019-05-30 17:52:50 -0400

    Regenerate master key and credentials file.  Clean up routes and update docker-compose

--- config/docker/dev/entrypoint.sh
+++ config/docker/dev/entrypoint.sh
@@ -3,6 +3,11 @@
 bundle check || bundle install --binstubs="$BUNDLE_BIN"
 
 if [ -z $1 ]; then
+  if [ -f tmp/pids/server.pid ]; then
+    echo "Stopping old server processes..."
+    kill -SIGINT "$(cat tmp/pids/server.pid)" >/dev/null 2>&1
+    rm tmp/pids/server.pid
+  fi
   echo "Starting rails server in $RAILS_ENV environment"
   rails s -b 0.0.0.0 -p 3000
 elif [[ $@ =~ ^(bundle|bundle install)$ ]]; then
@@ -11,6 +16,14 @@ elif [[ $@ =~ ^(bundle|bundle install)$ ]]; then
 elif [ $1 = "bundle" ]; then
   echo "Running bundle command: $@"
   "$@"
+elif [ $1 = "rake" ]; then
+  echo "Running rake command: $@"
+  shift
+  rake "$@"
+elif [ $1 = "rails" ]; then
+  echo "Running rails command: $@"
+  shift
+  rails "$@"
 else
   echo "Running rails command: $@"
   rails "$@"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,2 @@
 Rails.application.routes.draw do
-  jsonapi_resources :shows
-  jsonapi_resources :jobs
 end
